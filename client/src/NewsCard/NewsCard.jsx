f8a4aad19a8679de8ea0c013bf931e86c3b4b56f
lixuanqi <lixuanqi1995@gmail.com>
2018-06-27 01:30:45 +0800

    add Header and Footer and optimize NewsCard UI.

--- client/src/NewsCard/NewsCard.jsx
+++ client/src/NewsCard/NewsCard.jsx
@@ -9,22 +9,32 @@ class NewsCard extends React.Component{
 
   render() {
     return(
-      <div className="news-container" onClick={() => this.redirectToUrl(this.props.news.url)}>
-        <div className='row'>
-          <div class="col s12 m6">
-            <div class="card blue-grey darken-1">
-              <div class="card-content white-text">
-                <span class="card-title">Card Title</span>
-                <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
+      <div className="row">
+        <div className="col s0 m2 l3" />
+        <div className="col s12 m8 l6">
+          <div className="card large">
+            <div className="card-image">
+              <img src={this.props.news.urlToImage} alt=""/>
+              <span className="card-title">
+                {this.props.news.title}
+              </span>
+            </div>
+            <div className="card-content">
+              <p>
+                {this.props.news.description}
+              </p>
+            </div>
+            <div className="card-action">
+              <div>
+                {this.props.news.source != null && <div className="chip">{this.props.news.source}</div>}
+                {this.props.news.reason != null && <div className="chip">{this.props.news.reason}</div>}
+                {this.props.news.time != null && <div className="chip">{this.props.news.time}</div>}
               </div>
-              <div class="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
-              </div>
+              <a onClick={() => this.redirectToUrl(this.props.news.url)}>Read More</a>
             </div>
           </div>
         </div>
+        <div className="col s0 m2 l3" />
       </div>
     );
   }
--- client/src/NewsPanel/NewsPanel.jsx
+++ client/src/NewsPanel/NewsPanel.jsx
@@ -21,11 +21,20 @@ class NewsPanel extends React.Component{
         {
           title: 'News1',
           urlToImage: 'https://pic3.zhimg.com/v2-e505ddbe00a4677fa1f2e95b69f9ea4a.jpg',
-          description: 'This is a description.',
+          description: "The ruling sends a strong message that Trump has broad powers under immigration law to act to protect national security and that statements made during a campaign may not be legally determinative of the President's intent.",
           url: 'http://www.baidu.com',
           source: 'CNN',
           reason: 'Hot',
-          time: Date.now()
+          time: "2017-05-18"
+        },
+        {
+          title: 'News2',
+          urlToImage: 'https://pic3.zhimg.com/v2-e505ddbe00a4677fa1f2e95b69f9ea4a.jpg',
+          description: "The ruling sends a strong message that Trump has broad powers under immigration law to act to protect national security and that statements made during a campaign may not be legally determinative of the President's intent.",
+          url: 'http://www.baidu.com',
+          source: 'BBC',
+          reason: 'Hot',
+          time: "2017-05-18"
         }
       ]
     });
@@ -34,9 +43,9 @@ class NewsPanel extends React.Component{
   renderNews() {
     let news_list = this.state.news.map(function(news) {
       return(
-        <a className='list-group-item' key={news.digest} href="#">
+        <div className='list-group-item' key={news.digest}>
           <NewsCard news={news} />
-        </a>
+        </div>
       );
     });
 
