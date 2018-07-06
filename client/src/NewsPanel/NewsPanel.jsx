50630f45f24e1d29e437ac83c80cb55f491b9e7c
lixuanqi <lixuanqi1995@gmail.com>
2018-07-07 00:26:34 +0800

    add scroll loading feature.

--- client/src/NewsPanel/NewsPanel.jsx
+++ client/src/NewsPanel/NewsPanel.jsx
@@ -1,19 +1,29 @@
 import './NewsPanel.css';
 
 import React from 'react';
-
 import NewsCard from '../NewsCard/NewsCard'
+import _ from 'lodash';
 
 class NewsPanel extends React.Component{
   constructor() {
     super();
     this.state = {news:null};
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
     this.loadMoreNews();
+    this.loadMoreNews = _.debounce(this.loadMoreNews, 1000); // avoid trigger fetch too many times.
+    window.addEventListener('scroll', this.handleScroll);
   }
 
+  handleScroll() {
+    let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+    if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+      console.log('Loading more news');
+      this.loadMoreNews();
+    }
+  }
 
   loadMoreNews() {
     let request = new Request('http://localhost:5000/news', {
